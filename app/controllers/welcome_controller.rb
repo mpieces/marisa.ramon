99769e7434616c04e1d7b0e72d4a689f203f736e
Marisa Ramon <mer0522@gmail.com>
2015-11-04 12:19:58 -0600

    add new files

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,22 @@
+class WelcomeController < ApplicationController
+
+  # def index
+  # end
+
+  def home
+     @coverimage_class = true
+  end
+
+  def portfolio
+  end
+
+  def about
+  end
+
+  def resume
+  end
+
+  def contact
+  end
+
+end
