63fb2ef94ff42095cfd2f9d6e436176187cdb1ab
Marisa Ramon <mer0522@gmail.com>
2015-11-04 10:39:37 -0600

    add initial files

---
+++ MyPortfolio/app/controllers/welcome_controller.rb
@@ -0,0 +1,21 @@
+class WelcomeController < ApplicationController
+
+  # def index
+  # end
+
+  def home
+  end
+
+  def portfolio
+  end
+
+  def about
+  end
+
+  def resume
+  end
+
+  def contact
+  end
+
+end
