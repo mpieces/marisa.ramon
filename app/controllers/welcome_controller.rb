319bc9a06aa46f16069f2d5a58c08da562248999
Marisa Ramon <mer0522@gmail.com>
2015-11-25 16:01:43 -0600

    update resume with correct formatting, add links to projects code,...

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -12,23 +12,15 @@ class WelcomeController < ApplicationController
   end
 
   def about
+    @lox_class = true
   end
-
-  # def create
-  #   @resume = Resume.create(resume_params)
-  # end
 
   def resume
     @screen_class = true
-    # render file: "/marisa_ramon_resume.docx"
   end
 
   def contact
     @dallas_class = true
   end
 
-  # private
-  # def resume_params
-  #   params.require(:image).permit(:title, :body)
-  # end
 end
